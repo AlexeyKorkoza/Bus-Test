7226107d903c407c90855bdd1b99a9152c68b6c4
KrivichaninDenis <krivichaninds@gmail.com>
2016-06-14 22:50:07 +0300

    add geolocation + animation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,51 @@
-ymaps.ready(init);
+$(document).ready(function () {
 
-function init() {
-    var geolocation = ymaps.geolocation,
-        myMap = new ymaps.Map('map', {
-            center: [55, 34],
-            zoom: 10
-        }, {
-            searchControlProvider: 'yandex#search'
+    $('#geo').each(function () {
+        animationClick(this, 'tada');
+    });
+    document.getElementById('fromBlock').classList.add('animated','bounceInLeft');
+    document.getElementById('fromBlock').style.display = 'none'
+});
+
+function animationClick(element, animation) {
+    element = $(element);
+    element.click(
+        function () {
+            element.addClass('animated ' + animation);
+            //wait for animation to finish before removing classes
+            window.setTimeout(function () {
+                element.removeClass('animated ' + animation);
+            }, 2000);
+
         });
 }
+
+function geoFindMe() {
+    var output = document.getElementById("out");
+    var el = document.getElementById('fromBlock');
+
+    if (!navigator.geolocation) {
+        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+        return;
+    }
+
+    function success(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+        el.style.display = 'none';
+
+        output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+    };
+
+    function error() {
+        output.innerHTML = "Unable to retrieve your location";
+        el.style.display = 'block';
+
+    };
+
+    output.innerHTML = "<p>Locating…</p>";
+
+    navigator.geolocation.getCurrentPosition(success, error);
+}
+
