51e3901418da6ecc61323753fff734ef21f31584
KrivichaninDenis <krivichaninds@gmail.com>
2016-06-15 13:17:57 +0300

    fix bugs

--- js/script.js
+++ js/script.js
@@ -1,54 +1,51 @@
 $(document).ready(function () {
 
-    $('#geo').each(function () {
-        animationClick(this, 'tada');
-    });
     document.getElementById('fromBlock').classList.add('animated','bounceInLeft');
     document.getElementById('fromBlock').style.display = 'none';
 
     geoFindMe();
 });
 
-function animationClick(element, animation) {
-    element = $(element);
-    element.click(
-        function () {
-            element.addClass('animated ' + animation);
-            //wait for animation to finish before removing classes
-            window.setTimeout(function () {
-                element.removeClass('animated ' + animation);
-            }, 2000);
-
-        });
-}
-
 function geoFindMe() {
     var output = document.getElementById("out");
     var el = document.getElementById('fromBlock');
+    var timeoutVal = 10 * 1000 * 1000;
 
     if (!navigator.geolocation) {
         output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
         return;
+    }else{
+        navigator.geolocation.getCurrentPosition(success, error, { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 });
     }
 
     function success(position) {
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
+        var accuracy = position.coords.accuracy;
 
-        el.style.display = 'none';
 
-        output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-    };
+        output.innerHTML = '<p>Latitude is ' + latitude + '° <br>' +
+            'Longitude is ' + longitude + '°<br>Accuracy: '+ accuracy  +
+        '</p>';
+    }
 
-    function error() {
-        output.innerHTML = "Unable to retrieve your location";
+    function error(error) {
+        var errors = {
+            1: 'Permission denied',
+            2: 'Position unavailable',
+            3: 'Request timeout'
+        };
+
+        output.innerHTML = errors[error.code];
         el.style.display = 'block';
 
-    };
+    }
 
     output.innerHTML = "<p>Locating…</p>";
 
-    navigator.geolocation.getCurrentPosition(success, error);
+
+
+
 }
 
 function searchRoutes(){
