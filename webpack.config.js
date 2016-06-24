19a8b8b776dc6e1cc9fd05b5e1a8754fd9cc25e8
KrivichaninDenis <krivichaninds@gmail.com>
2016-06-24 12:55:24 +0300

    add bundle directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
-/**
- * Created by КРИВИЧАНИН on 24.06.2016.
- */
 module.exports = {
   entry: './js/script.js',
   output: {
-    path: __dirname,
+    path: './bundleDirectory',
     filename: 'bundle.js',
   },
   devServer: {
