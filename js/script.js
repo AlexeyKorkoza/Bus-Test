adec71e82779ce1e8c40fe7f33abe4ec9fd304ee
AlexeyKorkoza <Keane955@yandex.ru>
2016-07-01 18:09:25 +0300

    Add fake instruction

--- js/script.js
+++ js/script.js
@@ -197,3 +197,14 @@ submitButton.onclick = searchRoutes;
 watchOnMapButton.onclick = watchMapContainer;
 watchOnFormButton.onclick = watchFormContainer;
 submitButton.onkeydown = checkValues;
+
+/* accordion */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function(){
+    this.classList.toggle("active");
+    this.nextElementSibling.classList.toggle("show");
+  }
+}
